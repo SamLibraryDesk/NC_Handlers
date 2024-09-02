94c5f976b2f882ee54a7fd0b5bbf3d3701de3726
Sam <classicsbaa@gmail.com>
2024-09-02 19:27:32 +0300

    Update Rise of Nations Extended Edition.js

--- Ready/Rise of Nations Extended Edition/Rise of Nations Extended Edition.js
+++ Ready/Rise of Nations Extended Edition/Rise of Nations Extended Edition.js
@@ -23,19 +23,19 @@ Game.GameName = "Rise of Nations: Extended Edition";
 Game.MaxPlayersOneMonitor = 8;
 Game.MaxPlayers = 8;
 Game.FileSymlinkExclusions = [
-"steam_api.dll", "steam_interfaces.txt", "bhg_game_studios1M.wmv", "ms_game_studios_1M.wmv", 
-"skybox_intro_1440.wmv", "skybox_intro_1920.wmv", "opening.wmv", "d3d11.dll", "d3d11.ini", "dxgi.dll", "dxgi.ini"
+  "steam_api.dll", "steam_interfaces.txt", "bhg_game_studios1M.wmv", "ms_game_studios_1M.wmv",
+  "skybox_intro_1440.wmv", "skybox_intro_1920.wmv", "opening.wmv", "d3d11.dll", "d3d11.ini", "dxgi.dll", "dxgi.ini"
 ];
 Game.FileSymlinkCopyInstead = [
-"avutil-ttv-51.dll", "balancerules.txt", "bestimt.txt", "bighuge.txt", "conquestmotd.txt",
-"counterchart.txt", "d3d8xstub.dll", "d3dcompiler_46.dll", "d3dgl.dll", "dbghelp.dll", "dpnl.dll", 
-"dssl.dll", "Eulaxp1.dll", "game.txt", "graphics.txt", "labels.txt", "libmfxsw32.dll", "libmp3lame-ttv.dll", 
-"masks.txt", "motd.txt", "obsoletescriptfuncs.txt", "PidGenx.dll", "pp_unicows.dll", "rise xml.spp", 
-"rise xml.spp", "rtp32cb.dll", "rules.dat", "saveobjects.txt", "soundlist.txt", "soundtypes.txt", 
-"SteamNetLib.dll", "swresample-ttv-0.dll", "taunt_categories.txt", "taunts.txt", "unicows.dll", 
-"wmstubber.dll"
+  "avutil-ttv-51.dll", "balancerules.txt", "bestimt.txt", "bighuge.txt", "conquestmotd.txt",
+  "counterchart.txt", "d3d8xstub.dll", "d3dcompiler_46.dll", "d3dgl.dll", "dbghelp.dll", "dpnl.dll",
+  "dssl.dll", "Eulaxp1.dll", "game.txt", "graphics.txt", "labels.txt", "libmfxsw32.dll", "libmp3lame-ttv.dll",
+  "masks.txt", "motd.txt", "obsoletescriptfuncs.txt", "PidGenx.dll", "pp_unicows.dll", "rise xml.spp",
+  "rise xml.spp", "rtp32cb.dll", "rules.dat", "saveobjects.txt", "soundlist.txt", "soundtypes.txt",
+  "SteamNetLib.dll", "swresample-ttv-0.dll", "taunt_categories.txt", "taunts.txt", "unicows.dll",
+  "wmstubber.dll"
 ];
-Game.DirSymlinkCopyInstead = [ "terrain art", "tribes", "mapstyles", "ai" ];
+Game.DirSymlinkCopyInstead = ["terrain art", "tribes", "mapstyles", "ai"];
 Game.DirSymlinkCopyInsteadIncludeSubFolders = true;
 Game.DirExclusions = ["art"];
 Game.UseGoldberg = true;
@@ -49,7 +49,7 @@ Game.Description =
   "Use the old version of the game on steam (open steam -> Rise Of Nation -> Properties... -> Betas -> v1_10_old - 2014 version)\n" +
   "Requires 1 GB of space for each instance.\n" +
   "The game wouldn't resize the window less than 1024x768, so if you've 1920x1080 or less resolution you can't set 2 instance on 1 monitor,"
-+ " the possible way is to add and use higher custom desktop resolution (2732x1536 res can with 4 instances) .\n" +
+  + " the possible way is to add and use higher custom desktop resolution (2732x1536 res can with 4 instances) .\n" +
   "When the instances have repositioned press the END key to lock the input.\n" +
   "Create a game through 'Game Browser' and join with the others.\n" +
   "Press the END key again to unlock the input and you can use CTRL+Q to close Nucleus and all its instances."
@@ -158,10 +158,10 @@ Game.ProtoInput.FocusLoop_WM_NCACTIVATE = false;
 Game.ProtoInput.FocusLoop_WM_ACTIVATEAPP = false;
 Game.ProtoInput.FocusLoop_WM_SETFOCUS = false;
 Game.ProtoInput.FocusLoop_WM_MOUSEACTIVATE = false;
-Game.ProtoInput.BlockedMessages = [ 0x0008 ]; // Blocks WM_KILLFOCUS
+Game.ProtoInput.BlockedMessages = [0x0008]; // Blocks WM_KILLFOCUS
 
-Game.Play = function() {
-  
+Game.Play = function () {
+
   var savePath = (Context.SavePath = Context.GetFolder(Nucleus.Folder.InstancedGameFolder) + "\\d3d11.dll");
   var savePkgOrigin = System.IO.Path.Combine(Game.Folder, "d3d11.dll");
   System.IO.File.Copy(savePkgOrigin, savePath, true);
@@ -174,47 +174,47 @@ Game.Play = function() {
     var savePath = (Context.SavePath = Context.EnvironmentPlayer + "\\" + Context.UserProfileConfigPath + "\\PlayerProfile\\NucleusCoop.dat");
     var savePkgOrigin = System.IO.Path.Combine(Game.Folder, "NucleusCoop.dat");
     System.IO.File.Copy(savePkgOrigin, savePath, true);
-}
+  }
 
   var path = Context.EnvironmentPlayer + "\\" + Context.UserProfileConfigPath + "\\PlayerProfile\\NucleusCoop.dat";
   Context.ChangeXmlAttributeValue(path, "//PLAYER_NAME", "value", Context.Nickname);
 
   var path = Context.EnvironmentPlayer + "\\" + Context.UserProfileConfigPath + "\\PlayerProfile\\current_user.xml";
   Context.ChangeXmlAttributeValue(path, "//CURRENT_USER", "name", "NucleusCoop");
-   
+
   var height = Context.Height;
   var width = Context.Width;
-	
-  if (Context.Width < 1024) {var width = "1024"} else {var width = Context.Width}
-  if (Context.Height < 768) {var height = "768"} else {var height = Context.Height}
-		
+
+  if (Context.Width < 1024) { var width = "1024" } else { var width = Context.Width }
+  if (Context.Height < 768) { var height = "768" } else { var height = Context.Height }
+
   var Config = Context.GetFolder(Nucleus.Folder.InstancedGameFolder) + "\\d3d11.ini";
   Context.ModifySaveFile(Config, Config, Nucleus.SaveType.INI, [
-    new Nucleus.IniSaveInfo("Window.System","Borderless", "true"),
-	new Nucleus.IniSaveInfo("Window.System","OverrideRes", width + "x" + height)
+    new Nucleus.IniSaveInfo("Window.System", "Borderless", "true"),
+    new Nucleus.IniSaveInfo("Window.System", "OverrideRes", width + "x" + height)
   ]);
-  
-  var videoConfig = Context.EnvironmentPlayer + "\\" + Context.UserProfileConfigPath + "\\rise2.ini"; 
-  Context.ModifySaveFile(videoConfig, videoConfig, Nucleus.SaveType.INI, [    
-	new Nucleus.IniSaveInfo("RISE OF NATIONS","Fullscreen", "0"),
-    new Nucleus.IniSaveInfo("RISE OF NATIONS","SkipIntroMovies", "1"),
-	new Nucleus.IniSaveInfo("RISE OF NATIONS","Windowed Width", width),
-    new Nucleus.IniSaveInfo("RISE OF NATIONS","Windowed Height", height)
+
+  var videoConfig = Context.EnvironmentPlayer + "\\" + Context.UserProfileConfigPath + "\\rise2.ini";
+  Context.ModifySaveFile(videoConfig, videoConfig, Nucleus.SaveType.INI, [
+    new Nucleus.IniSaveInfo("RISE OF NATIONS", "Fullscreen", "0"),
+    new Nucleus.IniSaveInfo("RISE OF NATIONS", "SkipIntroMovies", "1"),
+    new Nucleus.IniSaveInfo("RISE OF NATIONS", "Windowed Width", width),
+    new Nucleus.IniSaveInfo("RISE OF NATIONS", "Windowed Height", height)
   ]);
 
   var fpscap = Context.Options["FPSCAP"];
-  if (fpscap == "Yes") {Context.ModifySaveFile(videoConfig, videoConfig, Nucleus.SaveType.INI, [ new Nucleus.IniSaveInfo("RISE OF NATIONS","VSync", "1")]);}
-  if (fpscap == "No") {Context.ModifySaveFile(videoConfig, videoConfig, Nucleus.SaveType.INI, [ new Nucleus.IniSaveInfo("RISE OF NATIONS","VSync", "0")]);}
- 
+  if (fpscap == "Yes") { Context.ModifySaveFile(videoConfig, videoConfig, Nucleus.SaveType.INI, [new Nucleus.IniSaveInfo("RISE OF NATIONS", "VSync", "1")]); }
+  if (fpscap == "No") { Context.ModifySaveFile(videoConfig, videoConfig, Nucleus.SaveType.INI, [new Nucleus.IniSaveInfo("RISE OF NATIONS", "VSync", "0")]); }
+
   var batPath = Context.GetFolder(Nucleus.Folder.InstancedGameFolder) + "\\Copy.bat";
   var batLines = [
     "@echo off",
     "title RoN COPIER",
     'xcopy "' +
-      (Context.RootInstallFolder + "\\art") +
-      '" "' +
-      Context.GetFolder(Nucleus.Folder.InstancedGameFolder) +
-      '\\art\\" /S /Y /E',
+    (Context.RootInstallFolder + "\\art") +
+    '" "' +
+    Context.GetFolder(Nucleus.Folder.InstancedGameFolder) +
+    '\\art\\" /S /Y /E',
   ];
   Context.WriteTextFile(batPath, batLines);
 
@@ -222,7 +222,7 @@ Game.Play = function() {
     Game.PauseBetweenContextAndLaunch = 50
     Context.RunAdditionalFiles(["all|" + Context.GetFolder(Nucleus.Folder.InstancedGameFolder) + "\\Copy.bat"], false, 0);
   }
-   
+
   var interfaces = Context.GetFolder(Nucleus.Folder.InstancedGameFolder) + "\\steam_interfaces.txt";
   var lines = [
     "SteamClient015",
@@ -248,27 +248,27 @@ Game.Play = function() {
   ];
   Context.WriteTextFile(interfaces, lines);
 
-    Game.ProtoInput.OnInputLocked = function() {
-      for (var i = 0; i < PlayerList.Count; i++) {
-        var player = PlayerList[i];
+  Game.ProtoInput.OnInputLocked = function () {
+    for (var i = 0; i < PlayerList.Count; i++) {
+      var player = PlayerList[i];
 
-        ProtoInput.SetDrawFakeCursor(player.ProtoInputInstanceHandle, true);
-        
-        ProtoInput.StartFocusMessageLoop(player.ProtoInputInstanceHandle, 0, true, true, true, true, true);
-  
-        System.Threading.Thread.Sleep(1000);
-  
-        ProtoInput.StopFocusMessageLoop(player.ProtoInputInstanceHandle);
-      }
-    };
-  
-    Game.ProtoInput.OnInputUnlocked = function() {
-      for (var i = 0; i < PlayerList.Count; i++) {
-        var player = PlayerList[i];
+      ProtoInput.SetDrawFakeCursor(player.ProtoInputInstanceHandle, true);
 
-        ProtoInput.SetDrawFakeCursor(player.ProtoInputInstanceHandle, false);
-  
-        ProtoInput.StartFocusMessageLoop(player.ProtoInputInstanceHandle, 0, true, true, true, true, true);
-      }
-    };
+      ProtoInput.StartFocusMessageLoop(player.ProtoInputInstanceHandle, 0, true, true, true, true, true);
+
+      System.Threading.Thread.Sleep(1000);
+
+      ProtoInput.StopFocusMessageLoop(player.ProtoInputInstanceHandle);
+    }
   };
+
+  Game.ProtoInput.OnInputUnlocked = function () {
+    for (var i = 0; i < PlayerList.Count; i++) {
+      var player = PlayerList[i];
+
+      ProtoInput.SetDrawFakeCursor(player.ProtoInputInstanceHandle, false);
+
+      ProtoInput.StartFocusMessageLoop(player.ProtoInputInstanceHandle, 0, true, true, true, true, true);
+    }
+  };
+};
