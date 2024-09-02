c16508f6b5ae57392abe2752cbbcca559884cdf7
Sam <classicsbaa@gmail.com>
2024-09-02 19:41:12 +0300

    Update Sudden Strike 2.js

--- Ready/Sudden Strike 2/Sudden Strike 2.js
+++ Ready/Sudden Strike 2/Sudden Strike 2.js
@@ -12,8 +12,7 @@
 
 /// Notes
 
-//var answers00 = ["Steam\\GoG", "Retail"];
-var answers0 = ["Yes", "No"];
+var answers0 = ["Yes", "Vertical & horizontal players only", "No"];
 var answers1 = ["60", "30"];
 var answers2 = ["Normal", "Fast"];
 var answers3 = ["Normal", "Slow", "Fast"];
@@ -30,7 +29,6 @@ var answers13 = ["Automatic", "640x480", "800x600", "960x1080", "1024x768", "115
 var answers14 = ["Automatic", "640x480", "800x600", "960x1080", "1024x768", "1152x864", "1280x720", "1280x960", "1280x1024", "1366x768", "1600x1024", "1920x1080"];
 var answers15 = ["Automatic", "640x480", "800x600", "960x1080", "1024x768", "1152x864", "1280x720", "1280x960", "1280x1024", "1366x768", "1600x1024", "1920x1080"];
 var answers16 = ["Automatic", "640x480", "800x600", "960x1080", "1024x768", "1152x864", "1280x720", "1280x960", "1280x1024", "1366x768", "1600x1024", "1920x1080"];
-//Game.AddOption("Game version", "", "GAMEVER", answers00);
 Game.AddOption("Preserve aspect ratios?", "This option will prevent stretching windows.", "PRERATIO", answers0);
 Game.AddOption("Limit FPS", "For less CPU consumption use lower FPS.", "FPSCAP", answers1);
 Game.AddOption("Game Speed", "This Option can be changed in the host menu.", "GAMESPEED", answers2);
@@ -51,6 +49,7 @@ Game.AddOption("Select your resolution for Player 12:", "", "P12Res", answers16)
 
 Game.FileSymlinkExclusions = ["DDraw.dll", "ddraw.ini", "dinput.dll", "winmm.dll", "intro.bik", "logo1.bik", "logo2.bik"];
 Game.DirSymlinkExclusions = ["movies", "SaveGames"];
+Game.FileSymlinkCopyInstead = [""];
 Game.UseNucleusEnvironment = true;
 Game.HandlerInterval = 100;
 Game.SymlinkExe = false;
@@ -60,23 +59,23 @@ Game.FileSymlinkCopyInstead = ["sudtest.ini"];
 Game.DirSymlinkCopyInstead = ["xchng"];
 Game.DirSymlinkCopyInsteadIncludeSubFolders = true;
 Game.HardcopyGame = false;
-Game.Description = "Create a multiplayer game with one and join with the others without setting any IP, if the hosted game not showing set IP by pressing type 127.0.0.1  \n" +
-"Press END to lock/unlock the inputs, While input is unlocked you can press CTRL+Q to close Nucleus and all of its instances."
+Game.Description = "Create a multiplayer game with one instance and join with the rest. If the hosted game is not showing, you can manually enter the localhost IP: 127.0.0.1\n\nPress END to lock and grant each player control, you can press END again to unlock input. While input is unlocked you can press CTRL+Q to close Nucleus and all of its instances."
 
 Game.ExecutableName = "game_exe.exe";
 Game.ExecutableContext = ["n2Game_Dll.dll"];
 Game.BinariesFolder = "code\\Release";
 Game.SteamID = "612520";
-Game.GUID = "Sudden Strike 2 Gold";
-Game.GameName = "Sudden Strike 2 Gold";
+Game.GUID = "Sudden Strike 2";
+Game.GameName = "Sudden Strike 2";
 Game.MaxPlayers = 12;
 Game.MaxPlayersOneMonitor = 12;
 
 Game.FakeFocus = true;
 Game.HookFocus = false;
-Game.ForceWindowTitle = true;
+// Game.ForceWindowTitle = true;
 Game.Hook.ForceFocusWindowName = "Sudden Strike version 2.0";
-Game.FakeFocusInterval =  100;
+// Game.HasDynamicWindowTitle = true;
+Game.FakeFocusInterval = 100;
 Game.Hook.ForceFocus = true;
 Game.DontResize = true;
 //Game.DontReposition = true;
@@ -97,7 +96,6 @@ Game.Hook.CustomDllEnabled = false;
 
 //USS deprecated options:
 
-Game.SupportsMultipleKeyboardsAndMice = false;
 Game.HookSetCursorPos = false;
 Game.HookGetCursorPos = false;
 Game.HookGetKeyState = false;
@@ -176,11 +174,9 @@ Game.ProtoInput.FocusLoop_WM_NCACTIVATE = false;
 Game.ProtoInput.FocusLoop_WM_ACTIVATEAPP = false;
 Game.ProtoInput.FocusLoop_WM_SETFOCUS = false;
 Game.ProtoInput.FocusLoop_WM_MOUSEACTIVATE = false;
-Game.ProtoInput.BlockedMessages = [ 0x0008]; // Blocks WM_KILLFOCUS
+Game.ProtoInput.BlockedMessages = [0x0008]; // Blocks WM_KILLFOCUS
 
-Game.Play = function() {
-		
-//  var gamever = Context.Options["GAMEVER"];
+Game.Play = function () {
   var preratio = Context.Options["PRERATIO"];
   var fpscap = Context.Options["FPSCAP"];
   var gamespeed = Context.Options["GAMESPEED"];
@@ -199,151 +195,131 @@ Game.Play = function() {
   var res10 = Context.Options["P11Res"];
   var res11 = Context.Options["P12Res"];
 
-  if (Context.PlayerID == 0) {var res = res0}
-  if (Context.PlayerID == 1) {var res = res1}
-  if (Context.PlayerID == 2) {var res = res2}
-  if (Context.PlayerID == 3) {var res = res3}
-  if (Context.PlayerID == 4) {var res = res4}
-  if (Context.PlayerID == 5) {var res = res5}
-  if (Context.PlayerID == 6) {var res = res6}
-  if (Context.PlayerID == 7) {var res = res7}
-  if (Context.PlayerID == 8) {var res = res8}
-  if (Context.PlayerID == 9) {var res = res9}
-  if (Context.PlayerID == 10) {var res = res10}
-  if (Context.PlayerID == 11) {var res = res11}
-  
+  if (Context.PlayerID == 0) { var res = res0 }
+  if (Context.PlayerID == 1) { var res = res1 }
+  if (Context.PlayerID == 2) { var res = res2 }
+  if (Context.PlayerID == 3) { var res = res3 }
+  if (Context.PlayerID == 4) { var res = res4 }
+  if (Context.PlayerID == 5) { var res = res5 }
+  if (Context.PlayerID == 6) { var res = res6 }
+  if (Context.PlayerID == 7) { var res = res7 }
+  if (Context.PlayerID == 8) { var res = res8 }
+  if (Context.PlayerID == 9) { var res = res9 }
+  if (Context.PlayerID == 10) { var res = res10 }
+  if (Context.PlayerID == 11) { var res = res11 }
+
   var exePath = Context.RootFolder;
-  
-  var savePath = (Context.SavePath = exePath + "\\game_exe.exe");
-  var savePkgOrigin = System.IO.Path.Combine(Context.RootInstallFolder, "game_exe.exe");
-  System.IO.File.Copy(savePkgOrigin, savePath, true);
-  
-  var savePath = (Context.SavePath = exePath + "\\n2Cad640.dll");
-  var savePkgOrigin = System.IO.Path.Combine(Context.RootInstallFolder, "n2Cad640.dll");
-  System.IO.File.Copy(savePkgOrigin, savePath, true);
-  
-  var savePath = (Context.SavePath = exePath + "\\n2Cad800.dll");
-  var savePkgOrigin = System.IO.Path.Combine(Context.RootInstallFolder, "n2Cad800.dll");
-  System.IO.File.Copy(savePkgOrigin, savePath, true);
-  
-  var savePath = (Context.SavePath = exePath + "\\n2Cad1024.dll");
-  var savePkgOrigin = System.IO.Path.Combine(Context.RootInstallFolder, "n2Cad1024.dll");
-  System.IO.File.Copy(savePkgOrigin, savePath, true);
-  
-  var savePath = (Context.SavePath = exePath + "\\n2Game_Dll.dll");
-  var savePkgOrigin = System.IO.Path.Combine(Context.RootInstallFolder, "n2Game_Dll.dll");
-  System.IO.File.Copy(savePkgOrigin, savePath, true);
-  
-  var savePath = (Context.SavePath = exePath + "\\n2Menu_dll.dll");
-  var savePkgOrigin = System.IO.Path.Combine(Context.RootInstallFolder, "n2Menu_dll.dll");
-  System.IO.File.Copy(savePkgOrigin, savePath, true);
-  
-  var savePath = (Context.SavePath = exePath + "\\n2Text_dll.dll");
-  var savePkgOrigin = System.IO.Path.Combine(Context.RootInstallFolder, "n2Text_dll.dll");
-  System.IO.File.Copy(savePkgOrigin, savePath, true);
-  
+
+  if (System.IO.File.Exists(Context.OrigRootFolder + "\\n2Menu_dll.dll")) {
+    Game.BinariesFolder = "";
+    Game.FileSymlinkCopyInstead = ["n2Cad640.dll", "n2Cad800.dll", "n2Cad1024.dll", "n2Game_Dll.dll", "n2Menu_dll.dll", "n2Text_dll.dll"];
+  } else {
+    var savePath = (Context.SavePath = exePath + "\\game_exe.exe");
+    var savePkgOrigin = System.IO.Path.Combine(Context.RootInstallFolder, "game_exe.exe");
+    System.IO.File.Copy(savePkgOrigin, savePath, true);
+
+    var savePath = (Context.SavePath = exePath + "\\n2Cad640.dll");
+    var savePkgOrigin = System.IO.Path.Combine(Context.RootInstallFolder, "n2Cad640.dll");
+    System.IO.File.Copy(savePkgOrigin, savePath, true);
+
+    var savePath = (Context.SavePath = exePath + "\\n2Cad800.dll");
+    var savePkgOrigin = System.IO.Path.Combine(Context.RootInstallFolder, "n2Cad800.dll");
+    System.IO.File.Copy(savePkgOrigin, savePath, true);
+
+    var savePath = (Context.SavePath = exePath + "\\n2Cad1024.dll");
+    var savePkgOrigin = System.IO.Path.Combine(Context.RootInstallFolder, "n2Cad1024.dll");
+    System.IO.File.Copy(savePkgOrigin, savePath, true);
+
+    var savePath = (Context.SavePath = exePath + "\\n2Game_Dll.dll");
+    var savePkgOrigin = System.IO.Path.Combine(Context.RootInstallFolder, "n2Game_Dll.dll");
+    System.IO.File.Copy(savePkgOrigin, savePath, true);
+
+    var savePath = (Context.SavePath = exePath + "\\n2Menu_dll.dll");
+    var savePkgOrigin = System.IO.Path.Combine(Context.RootInstallFolder, "n2Menu_dll.dll");
+    System.IO.File.Copy(savePkgOrigin, savePath, true);
+
+    var savePath = (Context.SavePath = exePath + "\\n2Text_dll.dll");
+    var savePkgOrigin = System.IO.Path.Combine(Context.RootInstallFolder, "n2Text_dll.dll");
+    System.IO.File.Copy(savePkgOrigin, savePath, true);
+  }
+
   Game.ExecutableToLaunch = exePath + "\\game_exe.exe";
-  
+
   var savePath = (Context.SavePath = exePath + "\\DDraw.dll");
   var savePkgOrigin = System.IO.Path.Combine(Game.Folder, "ddraw.dll");
   System.IO.File.Copy(savePkgOrigin, savePath, true);
-  
+
   var savePath = (Context.SavePath = exePath + "\\ddraw.ini");
   var savePkgOrigin = System.IO.Path.Combine(Game.Folder, "ddraw.ini");
   System.IO.File.Copy(savePkgOrigin, savePath, true);
-  
+
   var savePath = (Context.SavePath = exePath + "\\dinput.dll");
   var savePkgOrigin = System.IO.Path.Combine(Game.Folder, "dinput.dll");
   System.IO.File.Copy(savePkgOrigin, savePath, true);
-  
-  var savePath = (Context.SavePath = exePath + "\\winmm.dll");
-  var savePkgOrigin = System.IO.Path.Combine(Game.Folder, "winmm.dll");
-  System.IO.File.Copy(savePkgOrigin, savePath, true);
-  
+
   var savePath = (Context.SavePath = exePath + "\\sudtest.ini");
   var savePkgOrigin = System.IO.Path.Combine(Game.Folder, "sudtest.ini");
   System.IO.File.Copy(savePkgOrigin, savePath, true);
-  
+
   var sudPath = exePath + "\\sudtest.ini";
   var cncPath = exePath + "\\ddraw.ini";
 
-  if (preratio == "Yes") {
-	Context.ModifySaveFile(cncPath, cncPath, Nucleus.SaveType.INI, [new Nucleus.IniSaveInfo("ddraw","maintas", "true")]);
+  if (preratio == "Yes") { var prevr = "true" }
+  if (preratio == "No") { var prevr = "false" }
+  if (preratio == "Vertical & horizontal players only") {
+    if (Context.AspectRatioDecimal >= 3.55 && Context.AspectRatioDecimal <= 3.56 || Context.AspectRatioDecimal >= 0.87 && Context.AspectRatioDecimal <= 0.89) {
+      var prevr = "true"
+    }
+  }
 
-	var height = Context.Height;
-	var width = Context.Width;
-	var posY = Context.PosY;
-	var posX = Context.PosX;	
-	}
-
-  if (preratio == "No") {
-	Context.HideDesktop();
-
-	let HWidth = Math.ceil(Context.Width / 1.8);
-	let HPosX = Math.ceil((Context.Width - HWidth) / 2);
-	
-	let VHeight = Math.ceil(Context.Height / 1.3);
-	let VPosY = Math.ceil((Context.Height - VHeight) / 2);
-
-	if (Context.AspectRatioDecimal >= 3.55 && Context.AspectRatioDecimal <= 3.56) {
-	  var height = Context.Height;
-	  var width = HWidth;
-	  var posY = Context.PosY;
-	  var posX = HPosX;
-	  } else if (Context.AspectRatioDecimal >= 0.87 && Context.AspectRatioDecimal <= 0.89) {
-	  var height = VHeight;
-	  var width = Context.Width;
-	  var posY = VPosY;
-	  var posX = Context.PosX;
-	  } else {
-	  var height = Context.Height;
-	  var width = Context.Width;
-	  var posY = Context.PosY;
-	  var posX = Context.PosX;
-	  }
-	 }
+  var height = Context.Height;
+  var width = Context.Width;
+  var posY = Context.PosY;
+  var posX = Context.PosX;
 
   Context.ModifySaveFile(cncPath, cncPath, Nucleus.SaveType.INI, [
-	new Nucleus.IniSaveInfo("ddraw","windowed", "true"),
-	new Nucleus.IniSaveInfo("ddraw","border", "false"),
-	new Nucleus.IniSaveInfo("ddraw","singlecpu", "false"),
-	new Nucleus.IniSaveInfo("ddraw","maxfps", fpscap),
-	new Nucleus.IniSaveInfo("ddraw","height", height),
-	new Nucleus.IniSaveInfo("ddraw","width", width),
-	new Nucleus.IniSaveInfo("ddraw","posY", posY),
-	new Nucleus.IniSaveInfo("ddraw","posX", posX)
+    new Nucleus.IniSaveInfo("ddraw", "windowed", "true"),
+    new Nucleus.IniSaveInfo("ddraw", "border", "false"),
+    new Nucleus.IniSaveInfo("ddraw", "singlecpu", "false"),
+    new Nucleus.IniSaveInfo("ddraw", "devmode", "true"),
+    new Nucleus.IniSaveInfo("ddraw", "maxfps", fpscap),
+    new Nucleus.IniSaveInfo("ddraw", "maintas", prevr),
+    new Nucleus.IniSaveInfo("ddraw", "height", height),
+    new Nucleus.IniSaveInfo("ddraw", "width", width),
+    new Nucleus.IniSaveInfo("ddraw", "posY", posY),
+    new Nucleus.IniSaveInfo("ddraw", "posX", posX)
   ]);
 
-  if (gamespeed == "Fast") {var gS = "8";}
-  if (gamespeed == "Normal") {var gS = "5";}
-  if (mousespeed == "Fast") {var mS = "23";}
-  if (mousespeed == "Normal") {var mS = "17";}
-  if (mousespeed == "Slow") {var mS = "8";}
-  if (kbspeed == "Fast") {var kS = "25";}
-  if (kbspeed == "Normal") {var kS = "18";}
-  if (kbspeed == "Slow") {var kS = "8";}
+  if (gamespeed == "Fast") { var gS = "8"; }
+  if (gamespeed == "Normal") { var gS = "5"; }
+  if (mousespeed == "Fast") { var mS = "23"; }
+  if (mousespeed == "Normal") { var mS = "17"; }
+  if (mousespeed == "Slow") { var mS = "8"; }
+  if (kbspeed == "Fast") { var kS = "25"; }
+  if (kbspeed == "Normal") { var kS = "18"; }
+  if (kbspeed == "Slow") { var kS = "8"; }
 
-  if (res == "Automatic") { 
-	if (Context.Width >= 1024 && Context.Height >= 768) {
-	  var cad = '1024';
-	} else if (Context.Width >= 800 && Context.Height >= 600) {
-	  var cad = '800';
-	} else {
-	  var cad = '640';
-	}
+  if (res == "Automatic") {
+    if (Context.Width >= 1024 && Context.Height >= 768) {
+      var cad = '1024';
+    } else if (Context.Width >= 800 && Context.Height >= 600) {
+      var cad = '800';
+    } else {
+      var cad = '640';
+    }
   }
 
   Context.ModifySaveFile(sudPath, sudPath, Nucleus.SaveType.INI, [
-	new Nucleus.IniSaveInfo("Game", "SSDraw1", "n2cad" + cad + ".dll"),
-	new Nucleus.IniSaveInfo("Game", "KeyboardScroll", kS),
-	new Nucleus.IniSaveInfo("Game", "MouseScroll", mS),
-	new Nucleus.IniSaveInfo("Game", "MultiSpeed", gS),
-	new Nucleus.IniSaveInfo("Game", "VideoMode", "1"),
-	new Nucleus.IniSaveInfo("Game", "Player0", Context.Nickname)
+    new Nucleus.IniSaveInfo("Game", "SSDraw1", "n2cad" + cad + ".dll"),
+    new Nucleus.IniSaveInfo("Game", "KeyboardScroll", kS),
+    new Nucleus.IniSaveInfo("Game", "MouseScroll", mS),
+    new Nucleus.IniSaveInfo("Game", "MultiSpeed", gS),
+    new Nucleus.IniSaveInfo("Game", "VideoMode", "1"),
+    new Nucleus.IniSaveInfo("Game", "Player0", Context.Nickname)
   ]);
- 
-   Game.ProtoInput.OnInputLocked = function() {
-	for (var i = 0; i < PlayerList.Count; i++) {
+
+  Game.ProtoInput.OnInputLocked = function () {
+    for (var i = 0; i < PlayerList.Count; i++) {
       var player = PlayerList[i];
 
       ProtoInput.InstallHook(player.ProtoInputInstanceHandle, ProtoInput.Values.RegisterRawInputHookID);
@@ -367,7 +343,7 @@ Game.Play = function() {
       ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseWheelFilterID);
       ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseButtonFilterID);
       ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.KeyboardButtonFilterID);
-	  
+
       ProtoInput.StartFocusMessageLoop(player.ProtoInputInstanceHandle, 0, true, true, true, true, true);
 
       System.Threading.Thread.Sleep(1000);
@@ -376,7 +352,7 @@ Game.Play = function() {
     }
   };
 
-  Game.ProtoInput.OnInputUnlocked = function() {
+  Game.ProtoInput.OnInputUnlocked = function () {
     for (var i = 0; i < PlayerList.Count; i++) {
       var player = PlayerList[i];
 
