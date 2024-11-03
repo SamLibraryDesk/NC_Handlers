8847fca5c8ebe6fda29570ad3010e1c22b82f737
Sam <classicsbaa@gmail.com>
2024-11-03 15:02:03 +0300

    Update Commandos Behind Enemy Lines.js

--- WIP/Commandos Behind Enemy Lines/Commandos Behind Enemy Lines.js
+++ WIP/Commandos Behind Enemy Lines/Commandos Behind Enemy Lines.js
@@ -13,26 +13,25 @@
 /// Notes
     //Null
 
+var answers1 = ["Enable", "Disable"];
+Game.AddOption("HD Resolutions?", "Enable: Get rid of black borders and fit the exact monitor aspect.\n\n Disable: Good for low end PCs.", "HD", answers1);
+
 Game.UseNucleusEnvironment = true;
 Game.HandlerInterval = 100;
 Game.SymlinkExe = false;
 Game.SymlinkGame = true;
 Game.SymlinkFolders = false;
-Game.FileSymlinkExclusions = ["DDraw.dll", "ddraw.ini", "H_Intro.avi"];
+Game.FileSymlinkExclusions = ["DDraw.dll", "ddraw.ini", "H_Intro.avi", "commandos.exe"];
 //Game.FileSymlinkCopyInstead = [""];
 //Game.DirSymlinkExclusions = ["movies"];
-Game.DirSymlinkCopyInstead = ["Legacy"];
+// Game.DirSymlinkCopyInstead = ["Legacy"];
 Game.HardcopyGame = false;
-Game.Description = "If you've steam version change 'Comandos.exe' to 'commandos.exe' in your game directory '\Commandos Behind Enemy Lines\Legacy'. Start the game and wait until the instances reposition then press END to lock/unlock the inputs.";
-
-Game.ExecutableName = "Comandos.exe";
-Game.ExecutableToLaunch = "Legacy\\commandos.exe";
+Game.ExecutableName = "commandos.exe";
 Game.SteamID = "6800";
 Game.GUID = "Commandos Behind Enemy Lines";
 Game.GameName = "Commandos: Behind Enemy Lines";
 Game.DocumentsConfigPath = "Pyro Studios\\Commandos";
-//Game.BinariesFolder = "Legacy";
-//Game.UseGoldberg = true;
+Game.KillProcessesOnClose = ["mpserver"];
 Game.MaxPlayers = 6;
 Game.MaxPlayersOneMonitor = 6;
 
@@ -40,14 +39,13 @@ Game.FakeFocus = true;
 Game.HookFocus = false;
 Game.ForceWindowTitle = true;
 Game.Hook.ForceFocusWindowName = "COMMANDOS";
-Game.FakeFocusInterval =  5;
 Game.Hook.ForceFocus = true;
 Game.DontResize = true;
 Game.DontReposition = true;
 Game.HideTaskbar = true;
 Game.SetTopMostAtEnd = true;
-
-//Game.PauseBetweenProcessGrab = 10;
+Game.Description = "If you've steam version change 'Comandos.exe' to 'commandos.exe' . Start the game and wait until the instances reposition then press END to lock/unlock the inputs.";
+Game.PauseBetweenProcessGrab = 5;
 Game.PauseBetweenStarts = 10;
 
 Game.Hook.DInputForceDisable = false;
@@ -58,7 +56,6 @@ Game.Hook.CustomDllEnabled = false;
 
 //USS deprecated options:
 
-Game.SupportsMultipleKeyboardsAndMice = false;
 Game.HookSetCursorPos = false;
 Game.HookGetCursorPos = false;
 Game.HookGetKeyState = false;
@@ -79,70 +76,6 @@ Game.HookReRegisterRawInput = false;
 Game.HookReRegisterRawInputMouse = false;
 Game.HookReRegisterRawInputKeyboard = false;
 Game.DrawFakeMouseCursor = false;
-
-
-Game.Play = function() {
-
-  var savePath = (Context.SavePath = Context.GetFolder(Nucleus.Folder.InstancedGameFolder) + "\\Legacy\\DDraw.dll");
-  var savePkgOrigin = System.IO.Path.Combine(Game.Folder, "DDraw.dll");
-  System.IO.File.Copy(savePkgOrigin, savePath, true);
-  
-  var savePath = (Context.SavePath = Context.GetFolder(Nucleus.Folder.InstancedGameFolder) + "\\Legacy\\ddraw.ini");
-  var savePkgOrigin = System.IO.Path.Combine(Game.Folder, "ddraw.ini");
-  System.IO.File.Copy(savePkgOrigin, savePath, true);
-
-  var txtPath = Context.DocumentsPlayer + "\\" + Context.DocumentsConfigPath + "\\OUTPUT\\COMANDO.CFG";
-  
-  var dict = [
-    Context.FindLineNumberInTextFile(txtPath, ".NETGAMEPLAYER", Nucleus.SearchType.StartsWith) + "|.NETGAMEPLAYER " + Player.Nickname,
-    Context.FindLineNumberInTextFile(txtPath, ".SERVERADDRESS", Nucleus.SearchType.StartsWith) + "|.SERVERADDRESS 127.0.0.1"
-  ];	
-Context.ReplaceLinesInTextFile(txtPath, dict);
-
-  var txtPath = Context.DocumentsPlayer + "\\" + Context.DocumentsConfigPath + "\\OUTPUT\\COMANDO.CFG";
-	if (Context.Width > 1024 && Context.Height > 768) {
-	Context.FindLineNumberInTextFile(txtPath, ".SIZE [ .INITSIZE", Nucleus.SearchType.StartsWith) + "|.SIZE [ .INITSIZE 4 ]"
-} else if (Context.Width > 800 && Context.Height > 600) {
-	Context.FindLineNumberInTextFile(txtPath, ".SIZE [ .INITSIZE", Nucleus.SearchType.StartsWith) + "|.SIZE [ .INITSIZE 3 ]"
-} else if (Context.Width > 640 && Context.Height > 480) {
-	Context.FindLineNumberInTextFile(txtPath, ".SIZE [ .INITSIZE", Nucleus.SearchType.StartsWith) + "|.SIZE [ .INITSIZE 2 ]"
-} else {
-	Context.FindLineNumberInTextFile(txtPath, ".SIZE [ .INITSIZE", Nucleus.SearchType.StartsWith) + "|.SIZE [ .INITSIZE 1 ]"
-}
-
-Context.ReplaceLinesInTextFile(txtPath, dict);
-
-
-let HWidth = Math.ceil(Context.Width / 2);
-let HPosX = Math.ceil(Context.Width / 4);
-
-if (Context.AspectRatioDecimal >= 3.55 && Context.AspectRatioDecimal <= 3.56) {
-  var Config = Context.GetFolder(Nucleus.Folder.InstancedGameFolder) + "\\Legacy\\DDraw.ini";
-  Context.ModifySaveFile(Config, Config, Nucleus.SaveType.INI, [
-  new Nucleus.IniSaveInfo("ddraw","width", HWidth),
-  new Nucleus.IniSaveInfo("ddraw","posX", HPosX)
-  ]);
-} else {
-  var Config = Context.GetFolder(Nucleus.Folder.InstancedGameFolder) + "\\Legacy\\DDraw.ini"; 
-  Context.ModifySaveFile(Config, Config, Nucleus.SaveType.INI, [ 
-  new Nucleus.IniSaveInfo("ddraw","width", Context.Width),
-  new Nucleus.IniSaveInfo("ddraw","posX", Context.PosX)
-  ]);
-}
- 
-  var Config = Context.GetFolder(Nucleus.Folder.InstancedGameFolder) + "\\Legacy\\DDraw.ini"; 
-  Context.ModifySaveFile(Config, Config, Nucleus.SaveType.INI, [ 
-  new Nucleus.IniSaveInfo("ddraw","windowed", "true"),
-  new Nucleus.IniSaveInfo("ddraw","border", "false"),
-  new Nucleus.IniSaveInfo("ddraw","height", Context.Height),
-  new Nucleus.IniSaveInfo("ddraw","posY", Context.PosY),
-  new Nucleus.IniSaveInfo("ddraw","singlecpu", "false")
-  ]);
- 
- Context.RunAdditionalFiles(["1|" + Game.Folder + "\\StartMultInsCtrller.bat"], false, 0);
- Context.RunAdditionalFiles(["1|" + Context.GetFolder(Nucleus.Folder.InstancedGameFolder) + "\\Legacy\\mpserver.exe"], false, 0);
-
-};
 
 //ProtoInput
 
@@ -194,7 +127,194 @@ Game.ProtoInput.FocusLoop_WM_SETFOCUS = false;
 Game.ProtoInput.FocusLoop_WM_MOUSEACTIVATE = false;
 //Game.ProtoInput.BlockedMessages = [ 0x0008]; // Blocks WM_KILLFOCUS
 
-   Game.ProtoInput.OnInputLocked = function() {
+Game.Play = function () {
+
+  var savePath = (Context.SavePath = Context.GetFolder(Nucleus.Folder.InstancedGameFolder) + "\\commandos.exe");
+  var savePkgOrigin = System.IO.Path.Combine(Context.RootInstallFolder, "commandos.exe");
+  System.IO.File.Copy(savePkgOrigin, savePath, true);
+
+  Context.CopyScriptFolder(Context.GetFolder(Nucleus.Folder.InstancedGameFolder));
+
+  var hd = Context.Options["HD"];
+  if (hd == "Enable") {
+    // Context.PatchFileFindPattern(
+    //   System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+    //   System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+    //   "68 20 03 00 00 E8 0E 64 E6",
+    //   Context.Width,
+    //   2,
+    //   true
+    // );
+
+    // Context.PatchFileFindPattern(
+    //   System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+    //   System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+    //   "FF FF 8B 0D B8 12 78 00 6A 10 68 58 02 00 00",
+    //   Context.Height,
+    //   12,
+    //   true
+    // );
+
+    // Context.PatchFileFindPattern(
+    //   System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+    //   System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+    //   "68 20 03 00 00 E8 1A",
+    //   Context.Width,
+    //   2,
+    //   true
+    // );
+
+    // Context.PatchFileFindPattern(
+    //   System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+    //   System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+    //   "F8 FF 8B 0D B8 12 78 00 6A 10 68 58 02 00 00",
+    //   Context.Height,
+    //   12,
+    //   true
+    // );
+
+    // Context.PatchFileFindPattern(
+    //   System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+    //   System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+    //   "68 20 03 00 00 E8 4F",
+    //   Context.Width,
+    //   2,
+    //   true
+    // );
+
+    // Context.PatchFileFindPattern(
+    //   System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+    //   System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+    //   "00 8B 0D B8 12 78 00 6A 10 68 58 02 00 00",
+    //   Context.Height,
+    //   11,
+    //   true
+    // );
+
+    Context.PatchFileFindPattern(
+      System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+      System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+      "68 00 02 00 00 8B",//512x384
+      Context.Width,
+      2,
+      true
+    );
+
+    Context.PatchFileFindPattern(
+      System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+      System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+      "13 6A 10 68 80 01 00 00",//512x384
+      Context.Height,
+      5,
+      true
+    );
+
+    Context.PatchFileFindPattern(
+      System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+      System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+      "68 80 02 00 00 E8 2A",//640x480
+      Context.Width,
+      2,
+      true
+    );
+
+    Context.PatchFileFindPattern(
+      System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+      System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+      "FF 8B 0D B8 12 78 00 6A 10 68 E0 01 00 00",//640x480
+      Context.Height,
+      11,
+      true
+    );
+
+    Context.PatchFileFindPattern(
+      System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+      System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+      "68 20 03 00 00 E8 0E",//800x600
+      Context.Width,
+      2,
+      true
+    );
+
+    Context.PatchFileFindPattern(
+      System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+      System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+      "FF FF 8B 0D B8 12 78 00 6A 10 68 58 02 00 00",//800x600
+      Context.Height,
+      12,
+      true
+    );
+
+    // Context.PatchFileFindPattern(
+    //   System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+    //   System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+    //   "00",//1024x768
+    //   Context.Width,
+    //   2,
+    //   true
+    // );
+
+    // Context.PatchFileFindPattern(
+    //   System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+    //   System.IO.Path.Combine(Context.GetFolder(Nucleus.Folder.InstancedGameFolder), "commandos.exe"),
+    //   "FF",//1024x768
+    //   Context.Height,
+    //   12,
+    //   true
+    // );
+  }
+
+  if (Context.Width >= 1024 && Context.Height >= 768) {
+    var resSize = "4";
+  } else if (Context.Width >= 800 && Context.Height >= 600) {
+    var resSize = "3";
+  } else if (Context.Width >= 640 && Context.Height >= 480) {
+    var resSize = "2";
+  } else {
+    var resSize = "1";
+  }
+
+  if (System.IO.File.Exists(Context.DocumentsPlayer + "\\" + Context.DocumentsConfigPath + "\\OUTPUT\\Nucleus.txt")) {
+    var txtPath = Context.DocumentsPlayer + "\\" + Context.DocumentsConfigPath + "\\OUTPUT\\COMANDO.CFG";
+    var dict = [
+      Context.FindLineNumberInTextFile(txtPath, ".SIZE", Nucleus.SearchType.StartsWith) + "|.SIZE [ .INITSIZE " + resSize + " ]",
+      Context.FindLineNumberInTextFile(txtPath, ".NETGAMEPLAYER", Nucleus.SearchType.StartsWith) + "|.NETGAMEPLAYER " + Player.Nickname,
+      Context.FindLineNumberInTextFile(txtPath, ".SERVERADDRESS", Nucleus.SearchType.StartsWith) + "|.SERVERADDRESS 127.0.0.1"
+    ];
+    Context.ReplaceLinesInTextFile(txtPath, dict);
+  } else {
+    var ncPath = Context.DocumentsPlayer + "\\" + Context.DocumentsConfigPath + "\\OUTPUT\\Nucleus.txt";
+    var ncdict = ["Nucleus-Coop"];
+    Context.WriteTextFile(ncPath, ncdict);
+
+    var txtPath = Context.DocumentsPlayer + "\\" + Context.DocumentsConfigPath + "\\OUTPUT\\COMANDO.CFG";
+    var dict = [
+      "SAVA",
+      ".SONIDO [ .VOLUMEN 0.748031 .ACTIVO 1 ]",
+      ".SIZE [ .INITSIZE " + resSize + " ]",
+      ".PROFILE [ .USER 0 ]",
+      ".NETGAMEPLAYER " + Player.Nickname,
+      ".SERVERADDRESS 127.0.0.1"
+    ];
+    Context.WriteTextFile(txtPath, dict);
+  }
+
+  var cncPath = Context.GetFolder(Nucleus.Folder.InstancedGameFolder) + "\\ddraw.ini";
+  Context.ModifySaveFile(cncPath, cncPath, Nucleus.SaveType.INI, [
+    new Nucleus.IniSaveInfo("ddraw", "windowed", "true"),
+    new Nucleus.IniSaveInfo("ddraw", "border", "false"),
+    new Nucleus.IniSaveInfo("ddraw", "singlecpu", "false"),
+    // new Nucleus.IniSaveInfo("ddraw", "devmode", "true"),
+    new Nucleus.IniSaveInfo("ddraw", "maintas", "true"),
+    new Nucleus.IniSaveInfo("ddraw", "height", Context.Height),
+    new Nucleus.IniSaveInfo("ddraw", "width", Context.Width),
+    new Nucleus.IniSaveInfo("ddraw", "posY", Context.PosY),
+    new Nucleus.IniSaveInfo("ddraw", "posX", Context.PosX)
+  ]);
+
+  Context.RunAdditionalFiles(["1|" + Context.GetFolder(Nucleus.Folder.InstancedGameFolder) + "\\mpserver.exe"], false, 0);
+
+  Game.ProtoInput.OnInputLocked = function () {
     for (var i = 0; i < PlayerList.Count; i++) {
       var player = PlayerList[i];
 
@@ -219,11 +339,11 @@ Game.ProtoInput.FocusLoop_WM_MOUSEACTIVATE = false;
       ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseWheelFilterID);
       ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.MouseButtonFilterID);
       ProtoInput.EnableMessageFilter(player.ProtoInputInstanceHandle, ProtoInput.Values.KeyboardButtonFilterID);
-	  
+
     }
   };
 
-  Game.ProtoInput.OnInputUnlocked = function() {
+  Game.ProtoInput.OnInputUnlocked = function () {
     for (var i = 0; i < PlayerList.Count; i++) {
       var player = PlayerList[i];
 
@@ -248,3 +368,4 @@ Game.ProtoInput.FocusLoop_WM_MOUSEACTIVATE = false;
 
     }
   };
+};
